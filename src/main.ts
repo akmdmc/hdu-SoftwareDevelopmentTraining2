460bce87a658e35133bfb3792153c173d23a4b22
T <3428771711@qq.com>
2023-07-01 08:51:45 +0800

    new file:   .env 	new file:   .gitignore 	new file:   .vscode/extensions.json 	modified:   README.md 	new file:   env.d.ts 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   src/App.vue 	new file:   src/assets/base.css 	new file:   src/assets/logo.svg 	new file:   src/assets/main.css 	new file:   src/main.ts 	new file:   src/router/index.ts 	new file:   src/stores/counter.ts 	new file:   src/views/HomeView.vue 	new file:   tsconfig.app.json 	new file:   tsconfig.json 	new file:   tsconfig.node.json 	new file:   vite.config.ts

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import ArcoVue from '@arco-design/web-vue';
+import App from './App.vue'
+import router from './router'
+import '@arco-design/web-vue/dist/arco.css';
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(ArcoVue);
+
+app.mount('#app')
